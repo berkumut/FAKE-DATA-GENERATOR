e668e79414a38d6bfcc82f2dfcb4f5f31c1c2007
Berk Umut <cukucuku1905@gmail.com>
2026-09-15 21:39:49 +0300

    bugs fixed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from faker import Faker
 
+# LANGUAGE SETTİNGS
 languages = {
     "1": {
         "locale": "en_US",
@@ -32,6 +33,7 @@ languages = {
     },
 }
 
+# PRINTS THE SELECTION TO THE SCREEN
 print(""" SELECT A VALUE : 
 
     1- SELECT 1 FOR ENGLİSH
@@ -41,12 +43,15 @@ print(""" SELECT A VALUE :
     5- SELECT 5 FOR FRENCH
 """)
 
+# IT GIVES THE USER A CHOICE
 choice = input("PİCK A NUMBER : 1,2,3,4,5 :")
 
+# IF THEY GIVE A DIFFERENT NUMBER, I WILL WARN THEM AND LEAVE
 if choice not in languages:
     print("Please enter a valid value !!!")
     exit()
 
+# IT ASKS THE USER HOW MANY LETTERS THEY WANT
 while True:
     try:
         fakeLength = int(input("Enter how many you want to create :"))
@@ -56,8 +61,10 @@ while True:
         print("Please enter a valid value !!!")
         exit()
 
+# WE ARE ASSIGNING IT TO A VARIABLE
 fake = Faker(languages[choice]["locale"])
 
+# IT ROTATES AS MUCH AS THE USER WANTS
 for i in range(fakeLength):
 
     user = {
